a80fbad83fa39b34061c7f2e2f9ed4e2597f403e
Miguel Rivera Ospina <mikisbell@gmail.com>
2020-05-03 14:47:39 -0500

    ‘password2’

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'heroku_563f0f60139abe1' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'b40da50f464322' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '7aae4cd1e11859d' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
